a562e334fffd765b504d0ed63c12f54f95da6162
calebflores <calebflores1388@gmail.com>
2022-11-22 14:38:08 -0700

    splash functional

--- react-app/src/components/Splash/splash.js
+++ react-app/src/components/Splash/splash.js
@@ -1,13 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Redirect, useHistory } from 'react-router-dom'
+import { getAllQuestions } from '../../store/questions'
 
 function AllQuestions() {
     const dispatch = useDispatch()
+    const questions = useSelector(state => state.questions.Questions)
+    console.log('this is all my questuons', questions)
+
+
+    useEffect(() => {
+        dispatch(getAllQuestions())
+    }, [dispatch])
 
 
     return (
-        <h1>Hello Quorra</h1>
+        <div>
+            <h1>Hello Quorra</h1>
+            <div>
+                {questions?.map(question => (
+                    <div>
+                        <div>{question.title}</div>
+                        <div>{question.body}</div>
+                        <div>---------------------------------</div>
+                    </div>
+                ))}
+
+            </div>
+        </div>
     )
 
 }
